4ac2f43a0b672983c9850371d257a798a57a7aa3
Nick Allen <rutherford.t.b@gmail.com>
2022-09-21 00:28:53 -0500

    add pig

--- src/components/ThreeScene.jsx
+++ src/components/ThreeScene.jsx
@@ -1,8 +1,9 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useState, useEffect, Suspense} from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import Box from './ThreeBox';
 import ThreeForeground from './ThreeForeground';
 import './ThreeScene.jsx';
+import { Pig } from './ThreePig';
 
 const ThreeScene = () => {
     const [scroll, setScroll] = useState(0);
@@ -21,10 +22,11 @@ const ThreeScene = () => {
     console.log(scroll);
     return <div className='three-scene'>
         <ThreeForeground />
-        <Canvas style={{ top: scroll * .5, position: 'absolute' }}>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
-            <Box position={[0, 0, 0]} />
+        <Canvas style={{ top: 0, position: 'absolute' }}>
+                <ambientLight />
+                <pointLight position={[10, 10, 10]} />
+                <Box position={[3, scroll * .1, -3]} />
+                <Pig position={[-4,-5 + scroll * .1,-2]}/>
         </Canvas>
     </div>
 }
