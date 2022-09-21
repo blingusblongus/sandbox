1f407ff3c1878abc28092d52141e6a2874790be5
Nick Allen <rutherford.t.b@gmail.com>
2022-09-20 22:22:33 -0500

    init

---
+++ src/components/Parallax.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+export default () => {
+    const [offsetY, setOffsetY] = useState(0)
+
+    const handleScroll = () => {
+        console.log('scrolling');
+        setOffsetY(window.pageYOffset);
+    } 
+
+    const handleClick = () => {
+        console.log('click')
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    },[])
+
+    console.log(offsetY);
+    console.log('component mounted');
+
+    const styles = {
+        width: '200%',
+        position: 'absolute',
+        top: offsetY * -2,
+        zIndex: -1,
+        filter: 'blur(10px) opacity(10%)',
+        transform: 'scale(2) translateX(-10%)',
+        // objectFit: 'cover'
+    }
+
+    return <img onClick={handleClick} 
+        src="../../public/images/album_art.jpeg" 
+        style={styles} />
+}
---
+++ src/pages/markdown.md
@@ -0,0 +1,14 @@
+---
+layout: ../layouts/MarkdownLayout.astro
+title: Hello
+---
+
+# Header 1
+
+## Header 2
+
+Here's an article with some ***description*** stuff
+
+Ok, single line breaks are not enough in md files.
+
+<img src="/favicon.svg" width="100px"/>
